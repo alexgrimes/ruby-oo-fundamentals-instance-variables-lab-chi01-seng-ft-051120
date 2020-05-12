59d8eaf4894aa61f14821754ef9ab1ac3da42aac
Alex S Grimes <alex@alexgrimes.com>
2020-05-12 19:06:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
   
   #takes an argument of a dog's name and writes it to a variable
   def name=(dog_name)
-    @this_dogs_name = dog_name
+    this_dogs_name = dog_name
   end 
   
   #Reads the name 
