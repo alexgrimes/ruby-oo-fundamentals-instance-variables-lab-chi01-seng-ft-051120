c05fc2115b72683393cab9788484e76a8319692b
Alex S Grimes <alex@alexgrimes.com>
2020-05-12 18:57:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,12 @@
 require 'pry'
 class Dog 
+  
+  #takes an argument of a dog's name and writes it to a variable
   def name=(dog_name)
   this_dogs_name = dog_name
   end 
   
+  #Reads the name 
   def name 
     this_dogs_name
   end 
